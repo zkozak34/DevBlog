b20d2e6e350abc6ef366e80b69d0a4bdf3496ade
zenter <46599188+zkozak34@users.noreply.github.com>
2022-05-13 16:12:29 +0300

    add loading status in client side

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,13 +4,26 @@ import category from "./modules/category";
 import post from "./modules/post";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    isLoadedData: false,
+  },
+  getters: {
+    getIsLoadedData: (state) => state.isLoadedData,
+  },
+  mutations: {
+    setIsLoadedData(state, status) {
+      state.isLoadedData = status;
+    },
+  },
   actions: {
     initApp({ dispatch }) {
       dispatch("category/fetchList");
       dispatch("post/fetchList");
+    },
+    loadApp(context) {
+      if (context.rootState.category.isLoaded && context.rootState.post.isLoaded) {
+        context.commit("setIsLoadedData", true);
+      }
     },
   },
   modules: {
--- client/src/store/modules/post.js
+++ client/src/store/modules/post.js
@@ -4,22 +4,30 @@ export default {
   namespaced: true,
   state: {
     list: [],
+    isLoaded: false,
   },
   getters: {
     getList: (state) => state.list,
+    getIsLoaded: (state) => state.isLoaded,
   },
   mutations: {
     setList(state, list) {
       state.list = list;
     },
+    setIsLoaded(state, status) {
+      state.isLoaded = status;
+    },
   },
   actions: {
-    fetchList({ commit }) {
+    fetchList({ commit, dispatch }) {
+      commit("setIsLoaded", false);
       appAxios
         .get("posts/full")
         .then(({ data }) => {
           if (data.statusCode === 200 && data.data.length > 0) {
             commit("setList", data.data);
+            commit("setIsLoaded", true);
+            dispatch("loadApp", "", { root: true });
           }
         })
         .catch((e) => console.error(e));
